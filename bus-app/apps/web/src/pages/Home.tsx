c85c1dd99b5b00d931b590a8d31cb80d0810ff36
최현준 <choijjunii1@gmail.com>
2025-06-23 14:28:12 +0900

    Feat: 공통컴포넌트 Button,Input 생성

--- bus-app/apps/web/src/pages/Home.tsx
+++ bus-app/apps/web/src/pages/Home.tsx
@@ -1,8 +1,9 @@
+import { HomeContainer } from "@bus-app/ui/components/pages/home/HomeContainer";
+
 export default function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold text-blue-600">홈 페이지</h1>
-      <p className="mt-4 text-lg">여기는 KDT 프로젝트의 시작 페이지입니다.</p>
+      <HomeContainer />
     </div>
   );
 }
