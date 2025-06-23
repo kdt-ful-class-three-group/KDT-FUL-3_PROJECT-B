1084304074de4b63d704f67850157818573108e2
최현준 <choijjunii1@gmail.com>
2025-06-23 14:44:20 +0900

    Setting: express 기반 server 생성

---
+++ bus-app/apps/server/src/main.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to server!' });
+});
+
+const port = process.env.PORT
+const server = app.listen(port, () => {
+  console.log(`서버도는중... http://localhost:${port}/api`);
+});
+server.on('error', console.error);
