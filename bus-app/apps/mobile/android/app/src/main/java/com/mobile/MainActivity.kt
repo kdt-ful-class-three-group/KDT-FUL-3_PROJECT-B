be770d995f0ca505cb088cc4f7e1e5754a1c2aa4
김민지 <twoji3019@naver.com>
2025-07-01 13:41:03 +0900

    Build : react-native 실행 성공

--- bus-app/apps/mobile/android/app/src/main/java/com/mobile/MainActivity.kt
+++ bus-app/apps/mobile/android/app/src/main/java/com/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mobile
+package com.busapp.mobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- bus-app/apps/mobile/android/app/src/main/java/com/mobile/MainApplication.kt
+++ bus-app/apps/mobile/android/app/src/main/java/com/mobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mobile
+package com.busapp.mobile
 
 import android.app.Application
 import com.facebook.react.PackageList
